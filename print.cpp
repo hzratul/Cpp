03af56d45ef07143de97bc3ffedbd8a2ff420631
hzratul <hzratul94@gmail.com>
2023-12-21 00:36:26 +0600

    modified print.cpp

--- print.cpp
+++ print.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 int main(){
 	cout<<"Simple printing text cpp program\n";
-	cout<<"New line added.\n";
+	cout<<"New line added here.n";
 	cout<<"Another line is added.\n";
 	return 0;
 }
