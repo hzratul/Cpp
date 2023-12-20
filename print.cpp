2e64718b04af7987d79a1e2beb9c69153cd62931
hzratul <hzratul94@gmail.com>
2023-12-21 00:34:40 +0600

    removed syntax error in print.cpp

--- print.cpp
+++ print.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 int main(){
 	cout<<"Simple printing text cpp program\n";
-	cout<<"New line added.\n";
+	cout<<"New line has been added.\n";
 	cout<<"Another line is added.\n";
 	return 0;
 }
