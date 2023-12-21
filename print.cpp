f7ed5424b9737192e8f1e9f96deec1c00efe3ad6
hzratul <hzratul94@gmail.com>
2023-12-21 16:14:17 +0600

    print.cpp modified

--- print.cpp
+++ print.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 int main(){
 	cout<<"Simple printing text cpp program\n";
-	cout<<"New line has been added here.n";
-	cout<<"Another line is added.\n";
+    cout<<"New line has been added.n";
+	cout<<"I am enjoying using GitHub.";
 	return 0;
 }
