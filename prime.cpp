f8747114710a787d7e713c876f5e69bc6b70e89e
hzratul <hzratul94@gmail.com>
2023-12-21 19:24:34 +0600

    prime.cpp file added

---
+++ prime.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+using namespace std;
+bool isprime(int number)
+{	
+	bool prime = false;
+	if (number == 1) prime = true;
+	else if(number == 2)prime =  true;
+//	else if(number == 3)return true;
+
+	else if(number > 3 && prime == false)
+	{
+		int div;
+		for(div = 2; div < number; div++)
+		{
+			if(number % div == 0)
+			{
+				prime = false;
+				break;
+			}
+			else prime = true;
+		}
+	}
+	return prime;
+}
+
+int main()
+{
+	int count;
+	cout<<"Enter number : ";
+	int number;
+	cin>>number;
+
+	for(int n = 1; n <= number;n++ )
+	{
+		if(isprime(n)){
+			cout<<n<<"\t";
+			count++;
+			if(count % 5 == 0)cout<<'\n';
+		}
+	}
+	cout<<"\nPrime numbers found : "<<count<<'\n';
+	return 0;
+}
